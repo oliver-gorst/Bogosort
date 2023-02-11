6654128747895e8324ae1c9a427482b900c029a3
Oliver Gorst <oliver.gorst@outlook.com>
2023-02-10 22:30:21 -0500

    ChatGPT version of bogosort

--- bogosort.py
+++ bogosort.py
@@ -1,1 +1,15 @@
-#Test
+import random
+
+def is_sorted(arr):
+    for i in range(len(arr) - 1):
+        if arr[i] > arr[i + 1]:
+            return False
+    return True
+
+def bogosort(arr):
+    while not is_sorted(arr):
+        random.shuffle(arr)
+    return arr
+
+arr = [4, 2, 6, 1, 3, 5]
+print("Sorted array:", bogosort(arr))
